7de7809721fe244db0a1016bb8c789ae4c394716
takecare <rui@vazteixeira.org>
2017-02-26 23:39:48 +0000

    do not zip run configs

--- upload.sh
+++ upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-zip -r lambda.zip . -x ".*" -x "*/.*" -x "*.sample" -x "upload.sh"
+zip -r lambda.zip . -x ".*" -x "*/.*" -x "*.sample" -x "upload.sh" -x "run.sh" -x "run/*"
 aws lambda update-function-code --function-name github-webhooks-playground --zip-file fileb://lambda.zip
