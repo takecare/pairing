2287e8ec0dd163231ff9ea37b3421a6daeb4f450
takecare <rui@vazteixeira.org>
2017-02-26 22:55:07 +0000

    update run script

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #node-lambda run -H lambda.handler -j run/event.json -x run/context.json -f run/deploy.env
-export LOCAL_MODE=true && node-lambda run -H index.handler -j run/event.json -x run/context.json -f run/deploy.env
-
-#node-lambda run -H index.handler -j run/event.json -x run/context.json -f run/deploy.env
+export LOCAL_MODE=true
+node-lambda run -H lambda.handler -j run/event.json -x run/context.json -f run/deploy.env
